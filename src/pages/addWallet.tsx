8034780a81ad28694f0df982ae7f11dd59ca9a3b
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-09-14 00:08:40 +0900

    カスタムレイアウトの適用

--- src/pages/addWallet.tsx
+++ src/pages/addWallet.tsx
@@ -1,6 +1,7 @@
-import type { NextPage } from "next";
+import type { CustomNextPage } from "next";
 import { ComponentProps, useState } from "react";
 import axios, { AxiosRequestConfig } from "axios";
+import { Layout } from "src/layouts";
 
 type WalletDetail = {
   address: string;
@@ -10,7 +11,7 @@ type WalletDetail = {
   balance: string;
 };
 
-const MakeAccount: NextPage = () => {
+const MakeAccount: CustomNextPage = () => {
   const [myWallet, setMyWallet] = useState<WalletDetail>();
   const MakeAccount: ComponentProps<"form">["onSubmit"] = async (event) => {
     event.preventDefault();
@@ -23,23 +24,70 @@ const MakeAccount: NextPage = () => {
     setMyWallet(data);
   };
 
-  console.log(myWallet);
-
   return (
     <div className="p-20">
-      <h1>新しいウォレットの作成</h1>
-      <form onSubmit={MakeAccount}>
-        <button className="bg-green-500">Make you new wallet!!</button>
-      </form>
-
-      <hr />
-      <p>{`address:${myWallet?.address}`}</p>
-      <p>{`publicKey:${myWallet?.publicKey}`}</p>
-      <p>{`privateKey:${myWallet?.privateKey}`}</p>
-      <p>{`seed:${myWallet?.seed}`}</p>
-      <p>{`balance:${myWallet?.balance}`}</p>
+      <div className="container mx-auto flex w-full flex-col items-start  md:flex-row ">
+        <div className=" mb-16  flex flex-col items-center text-center md:mb-0 md:w-3/6 md:items-start md:text-left">
+          <h2 className="title-font mb-4 text-3xl font-medium text-gray-900 sm:text-4xl">
+            アカウントの作成
+          </h2>
+          <p className="mt-5 leading-relaxed">
+            ボタンを押してアカウントを作成しよう！
+            <br />
+            押して数秒後に情報が表示されるよ！
+            <br />
+            アカウント情報はメモをして大事に補完してね
+          </p>
+          <form onSubmit={MakeAccount} className="mt-5">
+            <button className="mt-5 block rounded border-0 bg-sky-500 py-2 px-6 text-lg text-white hover:bg-sky-600 focus:outline-none">
+              作成する
+            </button>
+          </form>
+        </div>
+        <div className="w-5/6 md:w-3/6 lg:w-full lg:max-w-lg">
+          <h2 className="title-font mb-4 text-3xl font-medium text-gray-900 sm:pl-6 sm:text-4xl">
+            アカウント情報
+          </h2>
+          <dl>
+            <div className="mt-6 flex-grow sm:mt-0 sm:pl-6">
+              <dt className="title-font mb-1 text-xl font-medium text-gray-900">
+                address
+              </dt>
+              <dd className="leading-relaxed"> {myWallet?.address}</dd>
+            </div>
+            <div className="mt-6 flex-grow sm:mt-0 sm:pl-6">
+              <dt className="title-font mb-1 text-xl font-medium text-gray-900">
+                publicKey
+              </dt>
+              <dd className="leading-relaxed">
+                <p>{myWallet?.publicKey}</p>{" "}
+              </dd>
+            </div>
+            <div className="mt-6 flex-grow sm:mt-0 sm:pl-6">
+              <dt className="title-font mb-1 text-xl font-medium text-gray-900">
+                privateKey
+              </dt>
+              <dd className="leading-relaxed"> {myWallet?.privateKey}</dd>
+            </div>
+            <div className="mt-6 flex-grow sm:mt-0 sm:pl-6">
+              <dt className="title-font mb-1 text-xl font-medium text-gray-900">
+                seed
+              </dt>
+              <dd className="leading-relaxed"> {myWallet?.seed}</dd>
+            </div>
+            <div className="mt-6 flex-grow sm:mt-0 sm:pl-6">
+              <dt className="title-font mb-1 text-xl font-medium text-gray-900">
+                balance
+              </dt>
+              <dd className="leading-relaxed"> {myWallet?.balance}</dd>
+            </div>
+          </dl>
+        </div>
+      </div>
     </div>
   );
 };
+
+MakeAccount.getLayout = (page) => <Layout>{page}</Layout>;
 
 export default MakeAccount;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
-import type { NextPage } from "next";
+import type { CustomNextPage } from "next";
 import { ComponentProps, useState } from "react";
 import axios, { AxiosRequestConfig } from "axios";
-import Link from "next/link";
-import { pagesPath } from "src/utils/$path";
+import { Layout } from "src/layouts";
 
 type WalletDetail = {
   address: string;
@@ -12,7 +11,7 @@ type WalletDetail = {
   balance: string;
 };
 
-const Home: NextPage = () => {
+const Home: CustomNextPage = () => {
   const wallet = {
     address: "",
     publicKey: "",
@@ -56,38 +55,107 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className="p-20">
-      <h1>hello world</h1>
-      <Link href={pagesPath.addWallet.$url()}>
-        <a className="bg-black text-white">MakeAccount</a>
-      </Link>
-      <form onSubmit={getMyWallet}>
-        <label htmlFor="seed">seed</label>
-        <input type="text" name="seed" id="seed" />
-        <button className="bg-green-500">Connect!!</button>
-      </form>
-
-      <hr />
-      <div>
-        <p>{`address:${myWallet?.address}`}</p>
-        <p>{`publicKey:${myWallet?.publicKey}`}</p>
-        <p>{`privateKey:${myWallet?.privateKey}`}</p>
-        <p>{`seed:${myWallet?.seed}`}</p>
-        <p>{`balance:${myWallet?.balance}`}</p>
+    <div>
+      <div className="container mx-auto flex w-full flex-col items-start  md:flex-row ">
+        <div className=" mb-16  flex flex-col items-center text-center md:mb-0 md:w-3/6 md:items-start md:text-left">
+          <h2 className="title-font mb-4 text-3xl font-medium text-gray-900 sm:text-4xl">
+            アカウント
+          </h2>
+          <p className="mt-5 leading-relaxed">
+            シードフレーズを入力して既存のアカウントデータを取得する
+          </p>
+          <form onSubmit={getMyWallet} className="mt-5">
+            <label htmlFor="seed" className="text-sm leading-7 text-gray-600">
+              シードフレーズ
+            </label>
+            <input
+              type="text"
+              name="seed"
+              id="seed"
+              className="w-full rounded border border-gray-300 bg-white py-1 px-3 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-sky-500 focus:ring-2 focus:ring-sky-200"
+            />
+            <button className="mt-5 block rounded border-0 bg-sky-500 py-2 px-6 text-lg text-white hover:bg-sky-600 focus:outline-none">
+              取得
+            </button>
+          </form>
+        </div>
+        <div className="w-5/6 md:w-3/6 lg:w-full lg:max-w-lg">
+          <h2 className="title-font mb-4 text-3xl font-medium text-gray-900 sm:pl-6 sm:text-4xl">
+            アカウント情報
+          </h2>
+          <dl>
+            <div className="mt-6 flex-grow sm:mt-0 sm:pl-6">
+              <dt className="title-font mb-1 text-xl font-medium text-gray-900">
+                address
+              </dt>
+              <dd className="leading-relaxed"> {myWallet?.address}</dd>
+            </div>
+            <div className="mt-6 flex-grow sm:mt-0 sm:pl-6">
+              <dt className="title-font mb-1 text-xl font-medium text-gray-900">
+                publicKey
+              </dt>
+              <dd className="leading-relaxed">
+                <p>{myWallet?.publicKey}</p>{" "}
+              </dd>
+            </div>
+            <div className="mt-6 flex-grow sm:mt-0 sm:pl-6">
+              <dt className="title-font mb-1 text-xl font-medium text-gray-900">
+                privateKey
+              </dt>
+              <dd className="leading-relaxed"> {myWallet?.privateKey}</dd>
+            </div>
+            <div className="mt-6 flex-grow sm:mt-0 sm:pl-6">
+              <dt className="title-font mb-1 text-xl font-medium text-gray-900">
+                seed
+              </dt>
+              <dd className="leading-relaxed"> {myWallet?.seed}</dd>
+            </div>
+            <div className="mt-6 flex-grow sm:mt-0 sm:pl-6">
+              <dt className="title-font mb-1 text-xl font-medium text-gray-900">
+                balance
+              </dt>
+              <dd className="leading-relaxed"> {myWallet?.balance}</dd>
+            </div>
+          </dl>
+        </div>
       </div>
-      <div className="border p-4">
-        <h2>XRPの送金</h2>
+      <div className="container mx-auto  px-5 py-24">
+        <h2 className="title-font mb-4 text-3xl font-medium text-gray-900 sm:text-4xl">
+          XRPの送金
+        </h2>
         {myWallet.address !== "" ? (
           <form onSubmit={transfer}>
             <div>
-              <label htmlFor="destination">送信先アドレス</label>
-              <input type="text" name="destination" id="destination" />
+              <label
+                htmlFor="destination"
+                className="text-sm leading-7 text-gray-600"
+              >
+                送信先アドレス
+              </label>
+              <input
+                type="text"
+                name="destination"
+                id="destination"
+                className="w-full rounded border border-gray-300 bg-white py-1 px-3 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-sky-500 focus:ring-2 focus:ring-sky-200"
+              />
             </div>
             <div>
-              <label htmlFor="amount">送金金額</label>
-              <input type="number" name="amount" id="amount" />
+              <label
+                htmlFor="amount"
+                className="text-sm leading-7 text-gray-600"
+              >
+                送金金額
+              </label>
+              <input
+                type="number"
+                name="amount"
+                id="amount"
+                className="w-full rounded border border-gray-300 bg-white py-1 px-3 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-sky-500 focus:ring-2 focus:ring-sky-200"
+              />
             </div>
-            <button>送金</button>
+            <button className="mt-5 block rounded border-0 bg-sky-500 py-2 px-6 text-lg text-white hover:bg-sky-600 focus:outline-none">
+              送金
+            </button>
           </form>
         ) : (
           <p>アカウントを取得してください</p>
@@ -96,5 +164,7 @@ const Home: NextPage = () => {
     </div>
   );
 };
+
+Home.getLayout = (page) => <Layout>{page}</Layout>;
 
 export default Home;
