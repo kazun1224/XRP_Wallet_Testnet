821cfc3bade0679bf20ec7ab353cb90df07dbed7
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-09-13 17:56:57 +0900

    XRPを送金するフォームの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,17 @@ type WalletDetail = {
 };
 
 const Home: NextPage = () => {
-  const [myWallet, setMyWallet] = useState<WalletDetail>();
+  const wallet = {
+    address: "",
+    publicKey: "",
+    privateKey: "",
+    seed: "",
+    balance: "",
+  };
+  const [myWallet, setMyWallet] = useState<WalletDetail>(wallet);
   const getMyWallet: ComponentProps<"form">["onSubmit"] = async (event) => {
     event.preventDefault();
     const seed: string = event.currentTarget.seed.value;
-    console.log(seed);
     const options: AxiosRequestConfig = {
       // here
       url: "/api/getWallet",
@@ -31,12 +37,30 @@ const Home: NextPage = () => {
     setMyWallet(data);
   };
 
-  console.log(myWallet);
+  const transfer: ComponentProps<"form">["onSubmit"] = async (event) => {
+    event.preventDefault();
+    const amount = event.currentTarget.amount.value;
+    const destination = event.currentTarget.destination.value;
+    const options: AxiosRequestConfig = {
+      url: "/api/MoneyTransfer",
+      method: "POST",
+      data: {
+        seed: myWallet?.seed,
+        destination,
+        amount,
+      },
+    };
+
+    const { data } = await axios(options);
+    setMyWallet({ ...myWallet, balance: data.balance });
+  };
 
   return (
     <div className="p-20">
       <h1>hello world</h1>
-      <Link href={pagesPath.addAccount.$url()}><a className="bg-black text-white">MakeAccount</a></Link>
+      <Link href={pagesPath.addWallet.$url()}>
+        <a className="bg-black text-white">MakeAccount</a>
+      </Link>
       <form onSubmit={getMyWallet}>
         <label htmlFor="seed">seed</label>
         <input type="text" name="seed" id="seed" />
@@ -44,11 +68,31 @@ const Home: NextPage = () => {
       </form>
 
       <hr />
-      <p>{`address:${myWallet?.address}`}</p>
-      <p>{`publicKey:${myWallet?.publicKey}`}</p>
-      <p>{`privateKey:${myWallet?.privateKey}`}</p>
-      <p>{`seed:${myWallet?.seed}`}</p>
-      <p>{`balance:${myWallet?.balance}`}</p>
+      <div>
+        <p>{`address:${myWallet?.address}`}</p>
+        <p>{`publicKey:${myWallet?.publicKey}`}</p>
+        <p>{`privateKey:${myWallet?.privateKey}`}</p>
+        <p>{`seed:${myWallet?.seed}`}</p>
+        <p>{`balance:${myWallet?.balance}`}</p>
+      </div>
+      <div className="border p-4">
+        <h2>XRPの送金</h2>
+        {myWallet.address !== "" ? (
+          <form onSubmit={transfer}>
+            <div>
+              <label htmlFor="destination">送信先アドレス</label>
+              <input type="text" name="destination" id="destination" />
+            </div>
+            <div>
+              <label htmlFor="amount">送金金額</label>
+              <input type="number" name="amount" id="amount" />
+            </div>
+            <button>送金</button>
+          </form>
+        ) : (
+          <p>アカウントを取得してください</p>
+        )}
+      </div>
     </div>
   );
 };
