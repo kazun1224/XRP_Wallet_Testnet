0747278016fdc1d9600c2d9eb73f101746c7fa6f
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-09-14 00:05:35 +0900

    Footerを作成

---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+export const Footer: FC = () => {
+  return (
+    <footer className="bg-sky-800">
+      <div className="py-10 text-center">
+        <p className="text-white">2022 XRP Ledger Test Net Wallet</p>
+      </div>
+    </footer>
+  );
+};
