2f24855484cd69fd1ba9c042a62e1df08a7fd307
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-09-24 17:44:34 +0900

    Headタグを追加してtitle、description、faviconを追加

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,13 +1,24 @@
 import { Footer } from "src/layouts/Footer";
 import { Header } from "src/layouts/Header";
 import { CustomLayout } from "next";
+import Head from "next/head";
 
 export const Layout: CustomLayout = (props) => {
   return (
-    <div className="body bg-slate-200">
-      <Header />
-      <main className="px-5 py-24">{props.children}</main>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>XRP Ledger Wallet</title>
+        <meta
+          name="description"
+          content="This is a wallet that trades XRP on XRP Ledger."
+        ></meta>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+      </Head>
+      <div className="body bg-slate-200">
+        <Header />
+        <main className="px-5 py-24">{props.children}</main>
+        <Footer />
+      </div>
+    </>
   );
 };
